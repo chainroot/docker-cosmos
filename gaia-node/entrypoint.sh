9522bda5c47945685b43e12c270b705e0bf31b39
mehran.eftekhari <mehran.eftekhari@noones.team>
2023-06-20 14:32:46 +0300

    working gaia

--- gaia-node/entrypoint.sh
+++ gaia-node/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -e
 
 CONFIG_PATH=$HOME/.gaia/config
 
@@ -7,21 +7,23 @@ envsubst < $HOME/config-sample/client.toml > $CONFIG_PATH/client.toml
 envsubst < $HOME/config-sample/config.toml > $CONFIG_PATH/config.toml
 
 if [ ! -f $HOME/.gaia/config/genesis.json ]; then 
-  if [ "$CLIENT__CHAIN_ID" = "cosmoshub-4" ]; then
-    wget https://raw.githubusercontent.com/cosmos/mainnet/master/genesis/genesis.cosmoshub-4.json.gz
-    gzip -d genesis.cosmoshub-4.json.gz
-    mv genesis.cosmoshub-4.json $HOME/.gaia/config/genesis.json
+  if [ $CLIENT__CHAIN_ID = "cosmoshub-4" ]; then
     #Download addrbook
     wget -O $CONFIG_PATH/addrbook.json https://dl2.quicksync.io/json/addrbook.cosmos.json
     #Download snapshot
     wget -O snapshot.tar.lz4 https://snapshots.polkachu.com/snapshots/cosmos/cosmos_$SNAPSHOT_BLOCK_HEIGHT.tar.lz4
-    #Extract data
-    lz4 -c -d cosmos_$SNAPSHOT_BLOCK_HEIGHT.tar.lz4  | tar -x -C $HOME/.gaia
-  elif [ "$CLIENT__CHAIN_ID" = "theta-testnet-001" ]; then
+    #Extract data and remove snapshot
+    lz4 -c -d snapshot.tar.lz4  | tar -x -C $HOME/.gaia && rm snapshot.tar.lz4
+    #Download genesis
+    wget https://raw.githubusercontent.com/cosmos/mainnet/master/genesis/genesis.cosmoshub-4.json.gz
+    gzip -d genesis.cosmoshub-4.json.gz
+    mv genesis.cosmoshub-4.json $HOME/.gaia/config/genesis.json
+
+  elif [ $CLIENT__CHAIN_ID = "theta-testnet-001" ]; then
     wget https://github.com/cosmos/testnets/raw/master/public/genesis.json.gz
     gzip -d genesis.json.gz
     mv genesis.json $CONFIG_PATH/genesis.json
-    #TODO download addrbook and snapshot
+    # TODO download addrbook and snapshot for theta-testnet-001
 
   fi
 fi
