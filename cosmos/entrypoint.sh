b207b5e88a2d8b89637854ce16b2b9d60df47345
OmidZamani <omidzamani2@gmail.com>
2023-12-22 02:19:24 +0330

    Refactor Dockerfile, entrypoint.sh, and check_version.sh for Variable Consistency
    - Updated Dockerfile to align with ARG and ENV variable changes for better build process.
    - Modified entrypoint.sh to synchronize environment setup with Dockerfile variables and ensure proper initialization of Gaia.
    - Revised check_version.sh to enhance version checking logic and ensure compatibility with updated Dockerfile and entrypoint.sh configurations.
    - Ensured all scripts are in sync with the latest variable definitions and dependencies.

--- cosmos/entrypoint.sh
+++ cosmos/entrypoint.sh
@@ -1,34 +1,30 @@
 #!/bin/sh -e
 
+MAINNET_CHAIN_ID=$(grep 'mainnet_chain_id' ./info | cut -d '=' -f2)
+TESTNET_CHAIN_ID=$(grep 'testnet_chain_id' ./info | cut -d '=' -f2)
 CONFIG_PATH=$HOME/.gaia/config
 
 mkdir -p $CONFIG_PATH
 
-envsubst < $HOME/config-sample/app.toml > $CONFIG_PATH/app.toml
-envsubst < $HOME/config-sample/client.toml > $CONFIG_PATH/client.toml
-envsubst < $HOME/config-sample/config.toml > $CONFIG_PATH/config.toml
+# Copy config files
+cp $HOME/config-sample/* $CONFIG_PATH/
 
 # first time setup
 if [ ! -f $CONFIG_PATH/genesis.json ]; then 
-  # mainnet
-  if [ $CLIENT__CHAIN_ID = "cosmoshub-4" ]; then
-    #Download addrbook
+  if [ "$MAINNET_CHAIN_ID" = "cosmoshub-4" ]; then
+    # Mainnet setup
     wget -O $CONFIG_PATH/addrbook.json https://dl2.quicksync.io/json/addrbook.cosmos.json
-    #Download snapshot
     wget -O snapshot.tar.lz4 https://snapshots.polkachu.com/snapshots/cosmos/cosmos_$SNAPSHOT_BLOCK_HEIGHT.tar.lz4
-    #Extract data and remove snapshot
     lz4 -c -d snapshot.tar.lz4  | tar -x -C $HOME/.gaia && rm snapshot.tar.lz4
-    #Download genesis
     wget https://raw.githubusercontent.com/cosmos/mainnet/master/genesis/genesis.cosmoshub-4.json.gz
     gzip -d genesis.cosmoshub-4.json.gz
     mv genesis.cosmoshub-4.json $CONFIG_PATH/genesis.json
-
-  # testnet
-  elif [ $CLIENT__CHAIN_ID = "theta-testnet-001" ]; then
+  elif [ "$TESTNET_CHAIN_ID" = "theta-testnet-001" ]; then
+    # Testnet setup
     wget https://github.com/cosmos/testnets/raw/master/public/genesis.json.gz
     gzip -d genesis.json.gz
     mv genesis.json $CONFIG_PATH/genesis.json
-    # TODO download addrbook and snapshot for theta-testnet-001
+    # TODO: Add addrbook and snapshot for theta-testnet-001
   fi
 fi
 
