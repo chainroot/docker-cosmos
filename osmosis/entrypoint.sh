530fe886c182cfd853154c3063495f2bb751cac6
OmidZamani <omidzamani2@gmail.com>
2023-12-26 10:38:14 +0330

    Update Osmosis entrypoint script for mainnet and testnet setup
    - Added logic to fetch chain IDs from the 'info' file for both mainnet and testnet.
    - Implemented download and setup of necessary files (snapshot, addrbook, genesis) for Osmosis mainnet.
    - Added download and setup steps for Osmosis testnet, including seeds and persistent peers configuration.
    - Ensured proper initialization of Osmosis node with 'osmosisd init' command based on the environment.
    - Initiated Osmosis daemon with 'osmosisd start'.

--- osmosis/entrypoint.sh
+++ osmosis/entrypoint.sh
@@ -9,8 +9,14 @@ mkdir -p $CONFIG_PATH
 
 if [ "$ENVIRONMENT" == "mainnet" ]; then
     osmosisd init --chain-id $MAINNET_CHAIN_ID
+    wget -O - https://snapshots.polkachu.com/snapshots/osmosis/osmosis_$SNAPSHOT_BLOCK_HEIGHT.tar.lz4 | lz4 -c -d - | tar -x -C $HOME/.osmosisd
+    wget -O $HOME/.osmosisd/config/addrbook.json https://snapshots.polkachu.com/addrbook/osmosis/addrbook.json
+    wget -O $HOME/.osmosisd/config/genesis.json https://github.com/osmosis-labs/networks/raw/main/osmosis-1/genesis.json
 elif [ "$ENVIRONMENT" == "testnet" ]; then
     osmosisd init --chain-id $TESTNET_CHAIN_ID
+    URL=$(wget -O - https://dl2.quicksync.io/json/osmosis.json | jq -r '.[] |select(.file=="osmotestnet-4-pruned")|select (.mirror=="Netherlands")|.url')
+    wget -O - $URL | lz4 -d | tar -x -C $HOME/.osmosisd
+    # TODO: Add addrbook and snapshot for osmo-test-4
 fi
 
 osmosisd start
