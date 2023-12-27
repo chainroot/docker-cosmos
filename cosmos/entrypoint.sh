378196e6e2e53b7c655c7aab30564f75c179190e
OmidZamani <omidzamani2@gmail.com>
2023-12-27 20:48:47 +0330

    Update entrypoint.sh for environment variable configuration
    - Set CHAIN_ID and SNAPSHOT_BLOCK_HEIGHT using environment variables
    - Initialize gaiad with the specified CHAIN_ID
    - Download and configure the genesis.json file based on CHAIN_ID
    - Remove unnecessary envsubst and config-sample usage
    - Ensure gaiad starts with the correct configuration

--- cosmos/entrypoint.sh
+++ cosmos/entrypoint.sh
@@ -1,35 +1,15 @@
-#!/bin/sh -e
+#!bin/sh -e
 
-CONFIG_PATH=$HOME/.gaia/config
+CHAIN_ID=${CHAIN_ID:-"cosmoshub-4"}
+SNAPSHOT_BLOCK_HEIGHT=${SNAPSHOT_BLOCK_HEIGHT:-"18459373"}
 
-mkdir -p $CONFIG_PATH
+gaiad init --chain-id $CHAIN_ID
 
-envsubst < $HOME/config-sample/app.toml > $CONFIG_PATH/app.toml
-envsubst < $HOME/config-sample/client.toml > $CONFIG_PATH/client.toml
-envsubst < $HOME/config-sample/config.toml > $CONFIG_PATH/config.toml
-
-# first time setup
-if [ ! -f $CONFIG_PATH/genesis.json ]; then 
-  # mainnet
-  if [ $CLIENT__CHAIN_ID = "cosmoshub-4" ]; then
-    #Download addrbook
-    wget -O $CONFIG_PATH/addrbook.json https://dl2.quicksync.io/json/addrbook.cosmos.json
-    #Download snapshot
-    wget -O snapshot.tar.lz4 https://snapshots.polkachu.com/snapshots/cosmos/cosmos_$SNAPSHOT_BLOCK_HEIGHT.tar.lz4
-    #Extract data and remove snapshot
-    lz4 -c -d snapshot.tar.lz4  | tar -x -C $HOME/.gaia && rm snapshot.tar.lz4
-    #Download genesis
-    wget https://raw.githubusercontent.com/cosmos/mainnet/master/genesis/genesis.cosmoshub-4.json.gz
-    gzip -d genesis.cosmoshub-4.json.gz
-    mv genesis.cosmoshub-4.json $CONFIG_PATH/genesis.json
-
-  # testnet
-  elif [ $CLIENT__CHAIN_ID = "theta-testnet-001" ]; then
-    wget https://github.com/cosmos/testnets/raw/master/public/genesis.json.gz
-    gzip -d genesis.json.gz
-    mv genesis.json $CONFIG_PATH/genesis.json
-    # TODO download addrbook and snapshot for theta-testnet-001
-  fi
+if [ "$CHAIN_ID" = "cosmoshub-4" ]; then
+    wget -O $HOME/.gaia/config/genesis.json https://raw.githubusercontent.com/cosmos/mainnet/master/genesis/genesis.cosmoshub-4.json.gz
+    gzip -d $HOME/.gaia/config/genesis.cosmoshub-4.json.gz
+    mv $HOME/.gaia/config/genesis.cosmoshub-4.json $HOME/.gaia/config/genesis.json
+elif [ "$CHAIN_ID" = "theta-testnet-001" ]; then
 fi
 
 gaiad start
