077a9f98f8d4631bf823663a5920926850da6d21
Moein <moein.fatehi@gmail.com>
2024-01-18 15:39:16 +0330

    fix: added Moniker

--- stride/entrypoint.sh
+++ stride/entrypoint.sh
@@ -1,29 +1,21 @@
-#!/bin/sh
+#!/bin/sh -e
 
-CONFIG_PATH=$HOME/.stride/config
+CHAIN_ID=${CHAIN_ID:-"stride-1"}
+SNAPSHOT_BLOCK_HEIGHT=${SNAPSHOT_BLOCK_HEIGHT:-"6965860"}
+MONIKER=${MONIKER:-"Chainroot"}
+strided init --chain-id $CHAIN_ID $MONIKER
 
-envsubst < $HOME/config-sample/app.toml > $CONFIG_PATH/app.toml
-envsubst < $HOME/config-sample/client.toml > $CONFIG_PATH/client.toml
-envsubst < $HOME/config-sample/config.toml > $CONFIG_PATH/config.toml
-
-if [ ! -f $CONFIG_PATH/genesis.json ]; then
-  # mainnet
-  if [ $CLIENT__CHAIN_ID = "stride-1" ]; then
-    wget -O $CONFIG_PATH/genesis.json https://raw.githubusercontent.com/Stride-Labs/mainnet/main/mainnet/genesis.json
-    wget -O $CONFIG_PATH/addrbook.json https://snapshots.polkachu.com/addrbook/stride/addrbook.json
-    #Download snapshot
+if [ "$CHAIN_ID" = "stride-1" ]; then
+    wget -O $HOME/.stride/config/genesis.json https://raw.githubusercontent.com/Stride-Labs/mainnet/main/mainnet/genesis.json
+    wget -O $HOME/.stride/config/addrbook.json https://snapshots.polkachu.com/addrbook/stride/addrbook.json
     wget -O snapshot.tar.lz4 https://snapshots.polkachu.com/snapshots/stride/stride_$SNAPSHOT_BLOCK_HEIGHT.tar.lz4
-    #Extract data
-    lz4 -c -d snapshot.tar.lz4  | tar -x -C $HOME/.stride
+    lz4 -c -d snapshot.tar.lz4 | tar -x -C $HOME/.stride
 
-  elif [$CLIENT__CHAIN_ID = "stride-testnet-1" ]; then
-    wget -O $CONFIG_PATH/genesis.json https://snapshots.polkachu.com/testnet-genesis/stride/genesis.json 
-    wget -O $CONFIG_PATH/addrbook.json https://snapshots.polkachu.com/testnet-addrbook/stride/addrbook.json
-    #Download snapshot
+elif [ "$CHAIN_ID" = "stride-testnet-1" ]; then
+    wget -O $HOME/.stride/config/genesis.json https://snapshots.polkachu.com/testnet-genesis/stride/genesis.json
+    wget -O $HOME/.stride/config/addrbook.json https://snapshots.polkachu.com/testnet-addrbook/stride/addrbook.json
     wget -O snapshot.tar.lz4 https://snapshots.polkachu.com/testnet-snapshots/stride/stride_$SNAPSHOT_BLOCK_HEIGHT.tar.lz4
-    #Extract data
-    lz4 -c -d snapshot.tar.lz4  | tar -x -C $HOME/.stride
-  fi
+    lz4 -c -d snapshot.tar.lz4 | tar -x -C $HOME/.stride
 fi
 
 strided start
